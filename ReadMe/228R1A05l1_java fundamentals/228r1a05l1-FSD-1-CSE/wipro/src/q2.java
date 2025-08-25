8b1b8ab5079c610bb7500c9f92df19df78f44b7e
B-Ravi01 <sairavikumar132@gmail.com>
2025-08-25 19:34:14 +0530

    Remove unrelated Java files

--- ReadMe/228R1A05l1_java fundamentals/228r1a05l1-FSD-1-CSE/wipro/src/q2.java
+++
@@ -1,9 +0,0 @@
-public class q2 {
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("No name provided.");
-            return;
-        }
-        System.out.println("Welcome " + args[0]);
-    }
-}
--- ReadMe/228R1A05l1_java fundamentals/228r1a05l1-FSD-1-CSE/wipro/src/q9.java
+++
@@ -1,9 +0,0 @@
-public class q9 {
-    public static void main(String[] args) {
-        char ch = args[0].charAt(0);
-        if (Character.isLowerCase(ch))
-            System.out.println("Given : " + ch + "\nConverted : " + Character.toUpperCase(ch));
-        else if (Character.isUpperCase(ch))
-            System.out.println("Given : " + ch + "\nConverted : " + Character.toLowerCase(ch));
-    }
-}
